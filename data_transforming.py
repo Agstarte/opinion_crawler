d39dbf3e1d22f9ebaaaa13c94fafc41bd9d6897a
Agstarte <aga1999.1@gmail.com>
2021-11-03 22:47:44 +0100

    data_transforming

--- data_transforming.py
+++ data_transforming.py
@@ -1,3 +1,5 @@
+import pickle
+
 import pandas as pd
 
 labels = {
@@ -41,14 +43,45 @@ def preprocessor(text):
             new_text += polish_characters[char]
         else:
             new_text += char
-
+    # czyszczenie tekstu ze wszystkiego poza literami i emotikonami
     emoticons = re.findall(r'(?::|;|=)(?:-)?(?:\)|\(|\(|D|P)', new_text)
-    new_text = re.sub(r'[^\w ]+', ' ', new_text.lower()) + " ".join(emoticons).replace('-', '')
+    new_text = re.sub(r'(#[^ ]+?$)|(#[^ ]+? )', '', new_text)
+    new_text = re.sub(r'([^a-z]+)', ' ', new_text.lower().replace('\\n', ' ')) \
+               + ' ' + ' '.join(emoticons).replace('-', '')
 
     return new_text
 
 
-print(preprocessor('fbdhasuj dfśąfółśą 1321 nfui456... ?? :) :*'))
+docs = [preprocessor(text) for text in docs]
+
+# lematyzacja wyrazów i tokenizacja
+import spacy
+nlp = spacy.load("pl_core_news_sm")
+
+# python -m spacy download pl_core_news_sm
+def lem_tok_docs(docs):
+    docs = [[word.lemma_ for word in nlp(text)] for text in docs]
+
+    print(docs)
+
+    pickle.dump(docs, open('lem_tok_docs.pkl', 'wb'), protocol=4)
+
+
+def load_lem_tok_docs():
+    return pickle.load(open('lem_tok_docs.pkl', 'rb'))
+
+
+docs = load_lem_tok_docs()
+print(docs)
+
+# stopwords
+
+stopwords = nlp.Defaults.stop_words
+stopwords.add(' ')
+
+docs = [[word for word in text if word not in stopwords] for text in docs if text]
+print(docs)
+
 
 #
 # # wektory cech
@@ -56,17 +89,17 @@ print(preprocessor('fbdhasuj dfśąfółśą 1321 nfui456... ?? :) :*'))
 # from sklearn.feature_extraction.text import CountVectorizer
 #
 # count = CountVectorizer()  # unigramowe, można zwiększyć ngram_range=2
-#
-# # print(docs)
-# # bag = count.fit_transform(docs)
 # #
+# # # print(docs)
+# bag = count.fit_transform(docs)
+#
 # # print(count.vocabulary_)
-#
-# # ważenie TF-IDF
-#
+# #
+# # # ważenie TF-IDF
+# #
 # from sklearn.feature_extraction.text import TfidfTransformer
 #
 # tf_idf = TfidfTransformer(use_idf=True,
 #                           norm='l2',
 #                           smooth_idf=True)
-# print(tf_idf.fit_transform(count.fit_transform(docs)).toarray())
+# print(tf_idf.fit_transform(bag).toarray())
